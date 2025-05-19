ed0cf7fc2374c5eef392fb3e7844677cb1d81b3b
sufyan <sufyanaslam2728@gmail.com>
2025-05-19 12:58:46 +0500

    initial setup of backend with node and sequelize

---
+++ index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const app = require("./app");
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
+});
